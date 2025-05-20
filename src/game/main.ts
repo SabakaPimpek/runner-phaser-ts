982a6edca66814285ea02e2e22851f176399feab
SabakaPimpek <kubaboczka1@gmail.com>
2025-05-20 13:27:34 +0200

    fixed css

--- src/game/main.ts
+++ src/game/main.ts
@@ -34,6 +34,10 @@ const config: Phaser.Types.Core.GameConfig = {
             },
         }
     },
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    }
 };
 
 const StartGame = (parent: string) => {
